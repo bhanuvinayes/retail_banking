bc6822b2f80a4f448da7b2f5b93e177c1d46ef25
Bhanu Vinay E S <bhanuvinayes@gmail.com>
2020-06-12 15:04:22 +0530

    commit on 'master'

--- application/routes.py
+++ application/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
