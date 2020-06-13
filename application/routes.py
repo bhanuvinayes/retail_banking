9eb70d0cf6a2b2318c2db540c8e8d20f25b077d7
Bhanu Vinay E S <bhanuvinayes@gmail.com>
2020-06-13 12:31:12 +0530

    Added login functionality

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,59 @@
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, request, session, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-@app.route('/')
-@app.route('/login')
+
+class Employees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uname = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        uname = request.form['uname']
+        password = request.form['pass']
+        user = Employees(uname = uname, password = password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration was successfull', category='info')
+        return redirect( url_for('login') )
+    return render_template('registration.html')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        return redirect( url_for('home') )
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        emp = Employees.query.filter_by(uname=username).first()
+        if emp == None:
+            flash('Invalid Credentials. Check User Name and Password', category='error')
+            return redirect( url_for('login') )
+        elif username == emp.uname and password == emp.password:
+            session['username'] = username
+            return redirect( url_for('home') )
+        else:
+            flash('Invalid Credentials. Check User Name and Password', category="error")
+
     return render_template('login.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return render_template('home.html')
+    else:
+        flash('You are logged out. Please login again to continue')
+        return redirect( url_for('login') )
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been successfully logged out.')
+    return redirect( url_for('login') )
