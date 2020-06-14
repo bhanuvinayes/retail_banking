87e19c117df207861eaa96dd27b9147af7b4fed6
Bhanu Vinay E S <bhanuvinayes@gmail.com>
2020-06-14 11:02:37 +0530

    Added add customer, search customer, delete customer functionality

--- application/routes.py
+++ application/routes.py
@@ -21,14 +21,28 @@ class Customers(db.Model):
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
+        email = request.form['email']
         uname = request.form['uname']
         password = request.form['pass']
+        cnfrm_password = request.form['cpass']
+
+        query = Employees.query.filter_by(uname = uname).first()
+
+        if query != None:
+            if uname == str(query.uname):
+                flash('Username already taken')
+                return redirect( url_for('registration') )
+        
+        if password != cnfrm_password:
+            flash('Password do not match')
+            return redirect( url_for('registration') )
+
         user = Employees(uname = uname, password = password)
         db.session.add(user)
         db.session.commit()
         flash('Registration was successfull', category='info')
         return redirect( url_for('login') )
-    return render_template('registration.html')
+    return render_template('emp_registration.html')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
@@ -86,22 +100,73 @@ def create_customer():
         flash('You are logged out. Please login again to continue')
         return redirect( url_for('login') )
 
-    return render_template('add_customer.html')
+    return render_template('create_customer.html')
 
 @app.route('/search_customer', methods=['GET', 'POST'])
 def search_customer():
-    if request.method == 'POST':
-        ssn_id = request.form['ssn_id']
-        customer = Customers.query.filter_by(ssn_id = ssn_id).first()
-        if customer == None:
-            flash('No customer with that ssn_id exists')
-            return redirect( url_for('search_customer') )
-        else:
-            flash('Following details found')
-            return render_template('customer_found.html', customer = customer)
+    if 'username' in session:
+        if request.method == 'POST':
+            ssn_id = request.form['ssn_id']
+            customer = Customers.query.filter_by(ssn_id = ssn_id).first()
+            if customer == None:
+                flash('No customer with that ssn_id exists')
+                return redirect( url_for('search_customer') )
+            else:
+                flash('Following details found')
+                return render_template('customer_found.html', customer = customer)
+    
+    else:
+        return redirect( url_for('login') )
     
     return render_template('search_customer.html')
 
 @app.route('/customer_found')
 def customer_found():
-    return render_template('customer_found.html')
+    if 'username' in session:
+        return render_template('customer_found.html')
+    else:
+        return redirect( url_for('login') )
+
+@app.route('/delete_customer', methods=['GET', 'POST'])
+def delete_customer():
+    if 'username' in session:
+        if request.method == 'POST':
+            ssn_id = request.form['ssn_id']
+            customer_id = request.form['customer_id']
+            customer_name = request.form['customer_name']
+            age = request.form['age']
+            address = request.form['address']
+
+            customer = Customers.query.filter_by(ssn_id = ssn_id).first()
+            if customer == None or str(customer.id) != customer_id or str(customer.ssn_id) != ssn_id or str(customer.cname) != customer_name or str(customer.age) != age or str(customer.address) != address:
+                flash('No customer with that that details found. Please enter correct details')
+                return redirect( url_for('delete_customer') )
+            else:
+                db.session.delete(customer)
+                db.session.commit()
+                flash('Successfully deleted customer')
+                return redirect( url_for('delete_customer') )
+    
+    return render_template('delete_customer.html')
+
+# TODO Complete view function
+@app.route('/update_customer')
+def update_customer():
+    return render_template('update_customer.html')
+
+# TODO Complete
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+
+@app.route('/delete_all', methods=['GET', 'POST'])
+def delete_all():
+    if request.method == 'POST':
+        db.session.query(Employees).delete()
+        db.session.commit()
+        db.session.query(Customers).delete()
+        db.session.commit()
+        flash('Deleted all customers record')
+        return render_template('delete_all.html')
+    return render_template('delete_all.html')
