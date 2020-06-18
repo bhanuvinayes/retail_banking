ccc349990efdbbc3246d1374548ba07abadc62e5
Bhanu Vinay E S <bhanuvinayes@gmail.com>
2020-06-18 09:49:52 +0530

    Final Update

--- application/routes.py
+++ application/routes.py
@@ -2,15 +2,16 @@ from application import app, db
 from flask import render_template, request, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import re
 
-# Table for employees
+# Creating Table for employees
 class Employees(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uname = db.Column(db.String(20))
     password = db.Column(db.String(20))
     date_created = db.Column(db.DateTime, default=datetime.now)
 
-# Table for customers
+# Creating Table for customers
 class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ssn_id = db.Column(db.Integer)
@@ -24,7 +25,7 @@ class Customers(db.Model):
     date = db.Column(db.DateTime, default=datetime.now)
 
 
-# Table for accounts
+# Creating Table for accounts
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cust_id = db.Column(db.Integer)
@@ -34,6 +35,7 @@ class Account(db.Model):
     acnt_msg =  db.Column(db.String(30))
     date = db.Column(db.DateTime, default=datetime.now)
 
+# Creating view function for registration page
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
@@ -53,18 +55,28 @@ def registration():
             flash('Password do not match')
             return redirect( url_for('registration') )
 
-        user = Employees(uname = uname, password = password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration was successfull', category='info')
-        return redirect( url_for('login') )
+        regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{10,20}$"
+        pattern = re.compile(regex)
+
+        match = re.search(pattern, password)
+        
+        if match:
+            user = Employees(uname = uname, password = password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Registration was successfull', category='info')
+            return redirect( url_for('login') )
+        else:
+            flash('Password should contain one Uppercase, one special character, one numeric character')
+            return redirect( url_for('registration') )
     return render_template('emp_registration.html')
 
 
+# Creating view function for login
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'username' in session:
+    if 'username' in session:                # Checking for session login
         return redirect( url_for('home') )
 
     if request.method == 'POST':
@@ -76,7 +88,7 @@ def login():
             flash('Invalid Credentials. Check User Name and Password', category='error')
             return redirect( url_for('login') )
         elif username == emp.uname and password == emp.password:
-            session['username'] = username
+            session['username'] = username  # saving session for login
             return redirect( url_for('home') )
         else:
             flash('Invalid Credentials. Check User Name and Password', category="error")
@@ -84,6 +96,7 @@ def login():
     return render_template('login.html')
 
 
+# Creating view function for Home page
 @app.route('/home')
 def home():
     if 'username' in session:
@@ -92,7 +105,7 @@ def home():
         flash('You are logged out. Please login again to continue')
         return redirect( url_for('login') )
 
-
+# Creating functionality for logout
 @app.route('/logout')
 def logout():
     session.pop('username', None)
@@ -100,17 +113,19 @@ def logout():
     return redirect( url_for('login') )
 
 
+# Creating view function for adding customer and writing relavent logic
 @app.route('/create_customer', methods=['GET', 'POST'])
 def create_customer():
-    if 'username' in session:
-        if request.method == 'POST':
+    if 'username' in session:                  # Checking session login
+        if request.method == 'POST':           # Checkig for POST request
             ssn_id = request.form['ssn_id']
-            cname = request.form['cname']
+            cname = request.form['cname']      # retriving data from form of html page
             age = request.form['age']
             address = request.form['address']
             state = request.form['state']
             city = request.form['city']
 
+            # Quering racord from customer table
             cust = Customers.query.filter_by( ssn_id = ssn_id ).first()
 
             if cust == None:
@@ -130,6 +145,7 @@ def create_customer():
     return render_template('create_customer.html')
 
 
+# Creating view function for search customer and writing relavent logic for displaying customer details
 @app.route('/search_customer', methods=['GET', 'POST'])
 def search_customer():
     if 'username' in session:
@@ -165,6 +181,7 @@ def search_customer():
     return render_template('search_customer.html')
 
 
+# Function to display searched customer details
 @app.route('/customer_found')
 def customer_found():
     if 'username' in session:
@@ -173,6 +190,7 @@ def customer_found():
         return redirect( url_for('login') )
 
 
+# Function to delete customer record
 @app.route('/delete_customer', methods=['GET', 'POST'])
 def delete_customer():
     if 'username' in session:
@@ -189,14 +207,14 @@ def delete_customer():
                 flash('No customer with that that details found. Please enter correct details')
                 return redirect( url_for('delete_customer') )
             else:
-                db.session.delete(customer)
+                db.session.delete(customer)     # deleting customer
                 db.session.commit()
                 if not accounts:
                     flash('Successfully deleted customer')
                     return redirect( url_for('delete_customer') )
                 
                 for account in accounts:
-                    db.session.delete(account)
+                    db.session.delete(account)   # deleting accounts for customer
                     db.session.commit()
                 flash('Successfully deleted customer')
                 return redirect( url_for('delete_customer') )
@@ -207,6 +225,7 @@ def delete_customer():
     return render_template('delete_customer.html')
 
 
+# View function for update customer
 @app.route('/update_customer', methods=['GET', 'POST'])
 def update_customer():
     if 'username' in session:
@@ -219,9 +238,10 @@ def update_customer():
     return render_template('update_customer.html')
 
 
+# Function to create account for existing customers
 @app.route('/create_account', methods=['GET', 'POST'])
 def create_account():
-    if 'username' in session:
+    if 'username' in session:               # Checking for session login
         if request.method == 'POST':
             savings = False
             current = False
@@ -236,6 +256,7 @@ def create_account():
                 flash('No customer exists with that id')
                 return redirect( url_for('create_account') )
             
+            # quring account details from accounts table
             acnt_cust_ids = Account.query.filter_by( cust_id = cust_id ).all()
             
             if not acnt_cust_ids:
@@ -255,6 +276,7 @@ def create_account():
                 if acnt_cust_id.acnt_type == 'Current': 
                     current = True
             
+            # Creatind a savings account for existing customer
             if account_type == 'Savings':
                 if savings == False:
                     account = Account( cust_id = int(cust_id), acnt_type = account_type, acnt_status = 'Active', bal = int(deposit_amount), acnt_msg = 'Account Created' )
@@ -263,6 +285,7 @@ def create_account():
                     flash('Savings Account Created')
                     return redirect( url_for('create_account') )
 
+            # Creating a current accoun for existing customer
             if account_type == 'Current':
                 if current == False:
                     account = Account( cust_id = int(cust_id), acnt_type = account_type, acnt_status = 'Active', bal = int(deposit_amount), acnt_msg = 'Account Created' )
@@ -272,7 +295,7 @@ def create_account():
                     return redirect( url_for('create_account') )
             
             if account_type == 'Savings':    
-                flash('Savings Account already created for this customer')
+                flash('Savings Account already created for this customer')     # Flashing relavent error message
                 return redirect( url_for('create_account') )
             else:
                 flash('Current Account already created for this customer')
@@ -283,21 +306,7 @@ def create_account():
 
     return render_template('create_account.html')
 
-
-@app.route('/delete_all', methods=['GET', 'POST'])
-def delete_all():
-    if request.method == 'POST':
-        db.session.query(Employees).delete()
-        db.session.commit()
-        db.session.query(Customers).delete()
-        db.session.commit()
-        db.session.query(Account).delete()
-        db.session.commit()
-        flash('Deleted all customers record')
-        return render_template('delete_all.html')
-    return render_template('delete_all.html')
-
-
+# Function to search for particular account
 @app.route('/search_accounts', methods=['GET', 'POST'])
 def search_accounts():
     if 'username' in session:
@@ -333,6 +342,7 @@ def search_accounts():
     return render_template('search_accounts.html')
 
 
+# Function to display all existing account details
 @app.route('/account_status')
 def account_status():
     if 'username' in session:
@@ -350,6 +360,7 @@ def account_status():
     return render_template('account_status.html')
 
 
+# Function to delete account from database
 @app.route('/delete_account', methods=['GET', 'POST'])
 def delete_account():
     if 'username' in session:
@@ -382,7 +393,7 @@ def delete_account():
 
     return render_template('delete_account.html')
 
-
+# Function for displaying all existing customer details
 @app.route('/customer_status')
 def customer_status():
     if 'username' in session:
@@ -390,7 +401,7 @@ def customer_status():
 
         if not customers:
             flash('There are customers')
-            return redirect( url_for('cusomer_status') )
+            return redirect( url_for('customer_status') )
         
         else:
             return render_template( 'customer_status.html', customers = customers )
@@ -402,6 +413,7 @@ def customer_status():
     return render_template('customer_status.html')
 
 
+# Function for depositing amount in particular account
 @app.route('/deposit', methods=['GET', 'POST'])
 def deposit():
     if 'username' in session:
@@ -438,7 +450,7 @@ def deposit():
     
     return render_template('Deposit.html')
     
-
+# Function to withdraw amount from particular account
 @app.route('/withdraw', methods=['GET', 'POST'])
 def withdraw():
     if 'username' in session:
@@ -476,6 +488,7 @@ def withdraw():
     return render_template('withdraw.html')
 
 
+# Function to transfer amount from either savings to current or current to savings
 @app.route('/transfer', methods=['GET', 'POST'])
 def transfer():
     if 'username' in session:
@@ -540,6 +553,7 @@ def transfer():
     return render_template('Transfer.html')
 
 
+# Function to display account statements
 @app.route('/account_statement', methods=['GET', 'POST'])
 def account_statement():
     if 'username' in session:
